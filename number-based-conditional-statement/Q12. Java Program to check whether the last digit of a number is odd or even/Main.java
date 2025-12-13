c7f2811daa47813850606fb1ed4392353d08bb99
SRIMAN S <147294360+Sriman777@users.noreply.github.com>
2025-12-13 09:27:18 +0530

    Add files via upload

---
+++ number-based-conditional-statement/Q12. Java Program to check whether the last digit of a number is odd or even/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int last = n % 10;
+        if (last % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
